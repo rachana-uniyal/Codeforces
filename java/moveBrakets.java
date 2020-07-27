e2709321278b4191c309cca9949a345b4f1c4d29
Rachana Uniyal <54140044+rachana-uniyal@users.noreply.github.com>
2020-07-27 17:08:35 +0530

    Create moveBrakets.java

---
+++ java/moveBrakets.java
@@ -0,0 +1,25 @@
+    import java.util.*;
+    public class cpbracket {
+        public static void main (String[] args) {
+            Scanner in = new Scanner(System.in);
+            int T = in.nextInt();
+            for (int t = 0; t < T; t++) {
+                int n = in.nextInt();
+                String s = in.next();
+                int c=0;
+                for(int i=0;i<n;i++){
+                    if(s.charAt(i)=='('){
+                        c++;
+                    }
+                    else{
+                        c--;
+                        if(c<0){
+                            c=0;
+                        }
+                    }
+                }
+                System.out.println(c);
+            }
+        }
+    }
+
